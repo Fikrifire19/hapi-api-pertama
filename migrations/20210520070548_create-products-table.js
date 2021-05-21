dbb0b811a9040b7e4b3be1fee9fafccb7b7310d5
fikri <muhamadfirki64@yahoo.co.id>
2021-05-21 10:37:13 +0700

    Create table products and knex migrate table

--- migrations/20210520070548_create-products-table.js
+++ migrations/20210520070548_create-products-table.js
@@ -3,14 +3,18 @@ exports.up = function(knex) {
   return knex.schema.createTable('products', (table) => {
       table.increments();
       table.string('product_name');
+      table.integer('price');
       table.integer('kategori_id');
       table.text('description');
-      table.integer('price');
       table.integer('inventory_id');
+      table.float('weight');
+      table.string('product_thumbmail');
+      table.string('product_image');
       table.integer('discount_id');
-  })
+      table.integer('stock');
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable('products');
 };
